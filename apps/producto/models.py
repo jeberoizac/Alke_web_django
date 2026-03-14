2a9da0314cef655d95ccfc33bfe0f220217626ea
Jurgen <jeberoizac@gmail.com>
2026-03-13 22:04:09 -0300

    se crea modelBase para auditorias y softDelete

--- apps/producto/models.py
+++ apps/producto/models.py
@@ -1,9 +1,46 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+# Importamos el modelo de usuario para las relaciones de auditoría
+User = get_user_model()
 
 # Create your models here.
 
-class Producto(models.Model):
+#1° Modelo de clase Base
+class BaseModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    #Metodo para auditar cambios
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="created_records"
+    )
+
+    updated_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="updated_records"
+    )
+
+    #Metodo para eliminar logico
+    def eliminar_logico(self):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    class Meta:
+        abstract = True
+
+#2° Modelo de clase Producto que hereda de BaseModel
+class Producto(BaseModel):
     ESTADO_CHOICES = [
         ('nuevo', 'Disponible (Nuevo) ✨'),
         ('instalado', 'Instalado en Cliente 🏠'),
@@ -27,14 +64,6 @@ class Producto(models.Model):
     fecha_ingreso = models.DateTimeField(default=timezone.now)
     fecha_salida = models.DateTimeField(null=True, blank=True)
 
-    # Campos para control de registros
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(null=True, blank=True)
-
-    def eliminar_logico(self):
-        self.deleted_at = timezone.now()
-        self.save()
-
+  
     def __str__(self):
         return f"{self.modelo} - {self.serie} ({self.get_estado_display()})"
